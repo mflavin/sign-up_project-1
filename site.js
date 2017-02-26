fa979c4fbb3c457b18a2db121e61fda197d251e6
mflavin <mflavin@hawk.iit.edu>
2017-02-26 15:42:03 -0600

    Changed class name.

--- site.js
+++ site.js
@@ -2,30 +2,30 @@ $.noConflict();
 
 jQuery(function($) {
   $('#email').on('focus', function() {
-    $('#input-email label').addClass('active');
+    $('#input-email label').addClass('hide');
     if( $('#name').val().length === 0 ) {
-      $('#input-name label').removeClass('active');
+      $('#input-name label').removeClass('hide');
     }
     if( $('#date').val().length === 0 ) {
-      $('#input-date label').removeClass('active');
+      $('#input-date label').removeClass('hide');
     }
   });
   $('#name').on('focus', function() {
-    $('#input-name label').addClass('active');
+    $('#input-name label').addClass('hide');
     if( $('#email').val().length === 0 ) {
-      $('#input-email label').removeClass('active');
+      $('#input-email label').removeClass('hide');
     }
     if( $('#date').val().length === 0 ) {
-      $('#input-date label').removeClass('active');
+      $('#input-date label').removeClass('hide');
     }
   });  
   $('#date').on('focus', function() {
-    $('#input-date label').addClass('active');
+    $('#input-date label').addClass('hide');
     if( $('#email').val().length === 0 ) {
-      $('#input-email label').removeClass('active');
+      $('#input-email label').removeClass('hide');
     }
     if( $('#name').val().length === 0 ) {
-      $('#input-name label').removeClass('active');
+      $('#input-name label').removeClass('hide');
     }
   });
 });
