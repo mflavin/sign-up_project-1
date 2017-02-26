b4418ae8b6a8073e580e15f12fa3a493833735a9
mflavin <mflavin@hawk.iit.edu>
2017-02-26 14:43:01 -0600

    Checks other textboxes to see if they are empty, if so they get the labels.

--- site.js
+++ site.js
@@ -3,17 +3,29 @@ $.noConflict();
 jQuery(function($) {
   $('#email').on('focus', function() {
     $('#input-email label').addClass('active');
-    $('#input-name label').removeClass('active');
-    $('#input-date label').removeClass('active');
+    if( $('#name').val().length === 0 ) {
+      $('#input-name label').removeClass('active');
+    }
+    if( $('#date').val().length === 0 ) {
+      $('#input-date label').removeClass('active');
+    }
   });
   $('#name').on('focus', function() {
     $('#input-name label').addClass('active');
-    $('#input-email label').removeClass('active');
-    $('#input-date label').removeClass('active');
+    if( $('#email').val().length === 0 ) {
+      $('#input-email label').removeClass('active');
+    }
+    if( $('#date').val().length === 0 ) {
+      $('#input-date label').removeClass('active');
+    }
   });  
   $('#date').on('focus', function() {
     $('#input-date label').addClass('active');
-    $('#input-email label').removeClass('active');
-    $('#input-name label').removeClass('active');
+    if( $('#email').val().length === 0 ) {
+      $('#input-email label').removeClass('active');
+    }
+    if( $('#name').val().length === 0 ) {
+      $('#input-name label').removeClass('active');
+    }
   });
 });
